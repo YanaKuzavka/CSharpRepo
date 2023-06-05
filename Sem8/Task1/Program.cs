4c581b306273d60c32814690fb97ea8cac75dcea
Yana <yanakuts21@gmail.com>
2023-06-05 17:40:20 +0300

    Add tasks from sem8-9

---
+++ Sem8/Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+
+
+{
+    int[,] matrix = new int[row, col];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+
+    }
+}
+
+void SortRow(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - j - 1; k++)
+                if (matrix[i,k] < matrix[i,k + 1])
+                {
+                    int temp = matrix[i,k];
+                    matrix[i,k] = matrix[i,k + 1];
+                    matrix[i,k + 1] = temp;
+                }
+        }
+    }
+
+}
+
+int[,] matrix = FillMatrix(ReadInt("Введите количество строк: "), ReadInt("Введите количество столбцов: "), 0, 20);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+
+SortRow(matrix);
+
+System.Console.WriteLine ("Матрица с упорядоченными элементами по убыванию:");
+PrintMatrix (matrix);
---
+++ Sem9/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и n. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до n.
+
+// M = 1; n = 15 -> 120
+// M = 4; n = 8. -> 30
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+static int SumNums(int M, int N)
+{
+    if (M == N)
+        return M;
+    else
+        return M + SumNums(M + 1, N);
+}
+
+int M = ReadInt("Введите положительное число M: ");
+int N = ReadInt("Введите положительно число N: ");
+int sum = SumNums(M, N);
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {M} до {N} = {sum}");
